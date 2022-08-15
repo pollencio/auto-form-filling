5716688a42549a90ee157fda1da98b7b4e3afad8
juanfel254 <jparrar@unal.edu.co>
2022-08-15 00:21:04 -0500

    formIds and baseUrl updated

--- src/fakeform.js
+++ src/fakeform.js
@@ -1,20 +1,18 @@
 export const baseUrl =
   //"https://docs.google.com/forms/d/e/1FAIpQLScVJ6BUWkkYeUveEaP0IuodShVPSdALgWCWSaZWHBKeHIlEBg/viewform?";
-  "https://docs.google.com/forms/d/e/1FAIpQLScVJ6BUWkkYeUveEaP0IuodShVPSdALgWCWSaZWHBKeHIlEBg/viewform?";
+  "https://docs.google.com/forms/d/e/1FAIpQLSewzGeYxCAd2mutLpVPjDvg0a1n0sEFPssxGkrFBWxFwkxyig/viewform?";
 
 export const formIds = {
-  project: 1731898983,
-  meetingType: 735665442,
-  date: 833089820, // must use format yyyy-mm-dd
-  meetingPlaceLink: 420605414,
-  fullName: 870981521,
-  documentId: 299847111,
-  email: 247412012,
-  gender: 1150607039,
-  disability: 1741621762,
-  cellphone: 879595708,
-  userType: 1571333861,
-  academicProgram: 1449262169,
-  faculty: 150600227,
-  //department: ,
+  project: 849749639, //
+  meetingType: 1976379347, //
+  date: 698082043, // 
+  meetingPlaceLink: 309080198, //
+  fullName: 1866984732, // If otro or egresado
+  documentId: 71221724, // 
+  documentId2: 1197924690, // If Otro or Egresado
+  email: 1592447237, // 
+  email2: 1090749917, // If otro or egresado
+  gender: 1452722238, // If otro or egresado
+  cellphone: 1093728203, // if otro or egresado
+  userType: 1084620211, //
 };
